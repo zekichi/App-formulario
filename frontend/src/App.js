960bf6d1fc61261065b76d16b938e186f019346b
Ezequiel <equintana779@gmail.com>
2025-08-04 18:04:41 -0300

    layout final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,23 @@
+// src/App.jsx
 import React from "react";
-import Formulario from "./components/Formulario";
+import Layout from "/components/Layout";
+import Formulario from "/components/Formulario";
+import ListadoFormularios from "/components/ListadoFormularios";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Formulario de Contacto</h1>
-      <Formulario />
-    </div>
-  );
-}
-
-import Formulario from "./components/Formulario";
-import ListadoFormularios from "./components/ListadoFormularios";
-
-function App(){
-  return(
-    <>
-      <div>
-        <h1>App de Formularios</h1>
+    <Layout>
+      <div className="mb-10">
         <Formulario />
-        <hr />
+        <hr className="my-6 border-[#bfae9b]" />
         <ListadoFormularios />
       </div>
-      <div className="bg-blue-500 text-white p-6 rounded">
+
+      <div className="bg-blue-500 text-white p-6 rounded text-center text-lg">
         ¡Tailwind está funcionando! 🎉
       </div>
-    </>
-  )
+    </Layout>
+  );
 }
 
 export default App;
--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -16,4 +16,6 @@ const Layout = ({ children }) => {
             </footer>
         </div>
     )
-}
+};
+
+export default Layout;
