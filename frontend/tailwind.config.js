951fc536ea27257b125fcffbeb8a41705422c613
Ezequiel <equintana779@gmail.com>
2025-08-18 15:07:28 -0300

    tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html"
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['"Libre Baskerville"', 'serif'],
+      },
+      colors: {
+        fondo: 'fdf6e3',
+        acento: 'b58900',
+        texto: '073642',
+        gris: 'eee8d5',
+        sombra: '586e75',
+      },
+    },
   },
   plugins: [],
 }
